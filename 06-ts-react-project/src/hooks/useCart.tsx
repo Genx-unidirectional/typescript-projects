872dfbfa1f4b67958cab242e488f18b5b1a50f3a
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-02-11 14:06:06 +0530

    created hooks to provide props from context

---
+++ 06-ts-react-project/src/hooks/useCart.tsx
@@ -0,0 +1,9 @@
+import { useContext } from "react";
+import CartContext from "../context/cartProvider";
+import { CartContextType } from "../context/cartProvider";
+
+const useCart = (): CartContextType => {
+  return useContext(CartContext);
+};
+
+export default useCart;
