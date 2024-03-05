34473471aa718c37fe9589721daab033c2da2192
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-05 17:15:25 +0530

    builder pattern with traditional method

--- 09-design-patterns/README.md
+++ 09-design-patterns/README.md
@@ -5,3 +5,10 @@
 Assume we have data , info , users which are array of objects , then we perform the function or any operation using those objects but what when that object is null we perform null checks so rather than performing lot's null checks we make similar special null object which behave in a neutral way
 
 In 01nullObjectPattern.ts we can clearly see that we can creative null object and which makes our code cleaner also not error prone and without bugs.
+
+# Builder Pattern
+
+Let's say we have class which creates object which is quite complex have lot's of method and properties init but lot's of properties in that class are optional so how here comes the builder pattern
+Builder is class which construct the object using the builder class
+It has function which step by step construct the object attributes
+Simply the builder class over the abstract class which where abstract class initializes the object and builder construct the object using it's function
---
+++ 09-design-patterns/build/js/02builderPattern.js
@@ -0,0 +1,36 @@
+"use strict";
+// let's consider the pizza we don't know what customer want cheese topping,  pepperoni toppings , mushrooms toppings so these three toppings may or may not be present and according to that we have to create the object
+// Making the class having the pizza requirements which fulfilled by pizzaBuilder
+class Pizza {
+    toppings;
+    size;
+    constructor(builder) {
+        this.toppings = builder.toppings;
+        this.size = builder.size;
+    }
+    describe() {
+        console.log(`The size of pizza is ${this.size}`);
+        console.log(`The toppings of pizza is ${this.toppings.join(" ")}`);
+    }
+}
+class PizzaBuilderImpl {
+    toppings;
+    size;
+    constructor(size) {
+        this.toppings = [];
+        this.size = size;
+    }
+    addToppings(toppings) {
+        this.toppings.push(toppings);
+        return this;
+    }
+    build() {
+        return new Pizza(this);
+    }
+}
+const pizza1 = new PizzaBuilderImpl("medium")
+    .addToppings("pepperoni")
+    .addToppings("cheese")
+    .addToppings("mushrooms")
+    .build();
+console.log(pizza1);
