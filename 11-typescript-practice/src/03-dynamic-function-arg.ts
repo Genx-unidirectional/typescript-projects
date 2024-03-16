fe89f06d88918267bcfe93614e0a645ef196cb3c
Ganesh kale <ganeshdoitnowh100@gmail.com>
2024-03-17 01:33:56 +0530

    pick and extract utilities usecase

--- 11-typescript-practice/src/03-dynamic-function-arg.ts
+++
@@ -1,8 +0,0 @@
-type TEvent =
-  | { type: "LOG_IN"; payload: { userId: string } }
-  | { type: "SIGN_OUT" };
-
-const sendEvent = (eventType: TEvent["type"], payLoad?: any) => {};
-
-sendEvent("LOG_IN", {});
-sendEvent("SIGN_OUT", { userId: "101" });
